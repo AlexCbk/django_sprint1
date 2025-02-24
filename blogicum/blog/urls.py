94d6e64fbf3cc3d5fa3bf79ba98287f9c86ca7b8
AlexCbk <togoq@icloud.com>
2025-02-24 20:18:51 +0300

    Add files via upload

---
+++ blogicum/blog/urls.py
@@ -0,0 +1,10 @@
+from blog import views
+
+from django.urls import path
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('posts/<int:id>/', views.post_detal, name='post_detal'),
+    path('category/<slug:category_slug>/', views.category_posts, name='category_posts'),
+]
---
+++ blogicum/pages/urls.py
@@ -0,0 +1,9 @@
+from pages import views
+
+from django.urls import path
+
+
+urlpatterns = [
+    path('pages/about/', views.about, name='about'),
+    path('pages/rules/', views.rules, name='rules'),
+]

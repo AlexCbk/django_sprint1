e1616f4d628028742cf96a67d0dd8c8bf285eeba
AlexCbk <togoq@icloud.com>
2025-02-25 06:11:55 +0300

    Update urls.py

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('posts/<int:id>/', include('blog.urls')),
     path('category/<slug:category_slug>/', include('blog.urls')),
-    path('pages/', include('pages.urls')),
+    path('pages/about/', include('pages.urls')),
+    path('pages/rules/', include('pages.urls')),
 ]

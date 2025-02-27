528f11f1a367ce94d1e52f14d7207a6fcca53be4
AlexCbk <togoq@icloud.com>
2025-02-27 07:21:08 +0300

    Update urls.py

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -1,10 +1,10 @@
-from pages import views
-
 from django.urls import path
+
+from .views import AboutView, RulesView
 
 app_name = 'pages'
 
 urlpatterns = [
-    path('about/', views.about, name='about'),
-    path('rules/', views.rules, name='rules'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('rules/', RulesView.as_view(), name='rules'),
 ]
